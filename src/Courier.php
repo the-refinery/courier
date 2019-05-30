db19f79cde6e8875bdb22736308bd70fb7b17d66
Brady <brady.bonnette@the-refinery.io>
2019-05-30 15:32:24 -0400

    WIP: Start of conversion

---
+++ src/Courier.php
@@ -0,0 +1,136 @@
+<?php
+/**
+ * Courier plugin for Craft CMS 3.x
+ *
+ * This is a CraftCMS 3 fork of the original Courier plugin. The original project can be found here: https://github.com/therefinerynz/courier
+ *
+ * @link      https://the-refinery.io
+ * @copyright Copyright (c) 2019 The Refinery
+ */
+
+namespace refinery\courier;
+
+use refinery\courier\services\CourierService as CourierServiceService;
+use refinery\courier\variables\CourierVariable;
+use refinery\courier\twigextensions\CourierTwigExtension;
+use refinery\courier\models\Settings;
+
+use Craft;
+use craft\base\Plugin;
+use craft\services\Plugins;
+use craft\events\PluginEvent;
+use craft\web\UrlManager;
+use craft\web\twig\variables\CraftVariable;
+use craft\events\RegisterUrlRulesEvent;
+
+use yii\base\Event;
+
+/**
+ * Class Courier
+ *
+ * @author    The Refinery
+ * @package   Courier
+ * @since     0.1.0
+ *
+ * @property  CourierServiceService $courierService
+ */
+class Courier extends Plugin
+{
+    // Static Properties
+    // =========================================================================
+
+    /**
+     * @var Courier
+     */
+    public static $plugin;
+
+    // Public Properties
+    // =========================================================================
+
+    /**
+     * @var string
+     */
+    public $schemaVersion = '1.1.0';
+
+    // Public Methods
+    // =========================================================================
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+        self::$plugin = $this;
+
+        Craft::$app->view->registerTwigExtension(new CourierTwigExtension());
+
+        Event::on(
+            UrlManager::class,
+            UrlManager::EVENT_REGISTER_SITE_URL_RULES,
+            function (RegisterUrlRulesEvent $event) {
+                $event->rules['siteActionTrigger1'] = 'courier/default';
+            }
+        );
+
+        Event::on(
+            UrlManager::class,
+            UrlManager::EVENT_REGISTER_CP_URL_RULES,
+            function (RegisterUrlRulesEvent $event) {
+                $event->rules['cpActionTrigger1'] = 'courier/default/do-something';
+            }
+        );
+
+        Event::on(
+            CraftVariable::class,
+            CraftVariable::EVENT_INIT,
+            function (Event $event) {
+                /** @var CraftVariable $variable */
+                $variable = $event->sender;
+                $variable->set('courier', CourierVariable::class);
+            }
+        );
+
+        Event::on(
+            Plugins::class,
+            Plugins::EVENT_AFTER_INSTALL_PLUGIN,
+            function (PluginEvent $event) {
+                if ($event->plugin === $this) {
+                }
+            }
+        );
+
+        Craft::info(
+            Craft::t(
+                'courier',
+                '{name} plugin loaded',
+                ['name' => $this->name]
+            ),
+            __METHOD__
+        );
+    }
+
+    // Protected Methods
+    // =========================================================================
+
+    /**
+     * @inheritdoc
+     */
+    protected function createSettingsModel()
+    {
+        return new Settings();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function settingsHtml(): string
+    {
+        return Craft::$app->view->renderTemplate(
+            'courier/settings',
+            [
+                'settings' => $this->getSettings()
+            ]
+        );
+    }
+}
