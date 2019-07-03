a32622079feb88ef95c4cceda1693c74ca5bfb0f
Brady <brady.bonnette@the-refinery.io>
2019-07-03 10:28:00 -0400

    WIP: cleanup blueprintemailevent

--- src/events/BlueprintEmailEvent.php
+++ src/events/BlueprintEmailEvent.php
@@ -2,22 +2,10 @@
 
 namespace refinery\courier\events;
 
-/**
- * ModelEvent class.
- *
- * @author Pixel & Tonic, Inc. <support@pixelandtonic.com>
- * @since 3.0
- */
 class BlueprintEmailEvent extends \yii\base\ModelEvent
 {
-    // Properties
-    // =========================================================================
-
-    /**
-     * @var bool Whether the model is brand new
-     */
-    public $blueprint;
-    public $renderVariables;
-    public $success;
-    public $errorMessage;
+  public $blueprint;
+  public $renderVariables;
+  public $success;
+  public $errorMessage;
 }
