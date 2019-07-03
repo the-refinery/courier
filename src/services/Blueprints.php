425cbf7b076a039ddac1715f4e0eadd792995e2e
Brady <brady.bonnette@the-refinery.io>
2019-07-03 11:37:05 -0400

    WIP: tabs to spaces

--- src/services/Blueprints.php
+++ src/services/Blueprints.php
@@ -13,95 +13,96 @@ use yii\base\Event;
 
 class Blueprints extends Component
 {
-	public function getAllBlueprints($criteria = null)
-	{
-		if(is_null($criteria))
-		{
-			$criteria = Blueprint::find();
-		}
+  public function getAllBlueprints($criteria = null)
+  {
+    if(is_null($criteria))
+    {
+      $criteria = Blueprint::find();
+    }
 
-		$records = $criteria
-			->all();
+    $records = $criteria
+      ->all();
 
-		$models = Courier::getInstance()
-			->modelPopulator
-			->populateModels(
-				$records,
-				\refinery\courier\models\Blueprint::class
-			);
+    $models = Courier::getInstance()
+      ->modelPopulator
+      ->populateModels(
+        $records,
+        \refinery\courier\models\Blueprint::class
+      );
 
-		return $models;
-	}
+    return $models;
+  }
 
-	public function getEnabledBlueprints()
-	{
-		$criteria = Blueprint::find()
-			->where(
-				[
-					'enabled' => true
-				]
-			);
 
-		return $this->getAllBlueprints($criteria);
-	}
+  public function getEnabledBlueprints()
+  {
+    $criteria = Blueprint::find()
+      ->where(
+        [
+          'enabled' => true
+        ]
+      );
 
-	public function saveBlueprint(BlueprintModel $model)
-	{
-		if ($model->id)
-			$record = Blueprint::findOne($model->id);
-		else {
-			$record = new Blueprint();
-		}
+    return $this->getAllBlueprints($criteria);
+  }
 
-		$model->validate();
+  public function saveBlueprint(BlueprintModel $model)
+  {
+    if ($model->id)
+      $record = Blueprint::findOne($model->id);
+    else {
+      $record = new Blueprint();
+    }
 
-		if ($model->hasErrors()) {
-			return false;
-		}
+    $model->validate();
 
-		// Populate the blueprint record
-		$record->name 					= $model->name;
-		$record->description 		= $model->description;
-		$record->enabled 				= $model->enabled;
-		$record->emailSubject 	= $model->emailSubject;
-		$record->toEmail 				= $model->toEmail;
-		$record->toName 				= $model->toName;
-		$record->fromEmail 			= $model->fromEmail;
-		$record->fromName 			= $model->fromName;
-		$record->replyToEmail		= $model->replyToEmail;
-		$record->ccEmail 				= $model->ccEmail;
-		$record->bccEmail 			= $model->bccEmail;
-		$record->eventTriggers	= $model->eventTriggers;
-		$record->htmlEmailTemplatePath 	= $model->htmlEmailTemplatePath;
-		$record->textEmailTemplatePath 	= $model->textEmailTemplatePath;
-		$record->eventTriggerConditions = $model->eventTriggerConditions;
+    if ($model->hasErrors()) {
+      return false;
+    }
 
-		$record->save(false);
-		$model->id = $record->id;
+    // Populate the blueprint record
+    $record->name           = $model->name;
+    $record->description    = $model->description;
+    $record->enabled        = $model->enabled;
+    $record->emailSubject   = $model->emailSubject;
+    $record->toEmail        = $model->toEmail;
+    $record->toName         = $model->toName;
+    $record->fromEmail      = $model->fromEmail;
+    $record->fromName       = $model->fromName;
+    $record->replyToEmail   = $model->replyToEmail;
+    $record->ccEmail        = $model->ccEmail;
+    $record->bccEmail       = $model->bccEmail;
+    $record->eventTriggers  = $model->eventTriggers;
+    $record->htmlEmailTemplatePath  = $model->htmlEmailTemplatePath;
+    $record->textEmailTemplatePath  = $model->textEmailTemplatePath;
+    $record->eventTriggerConditions = $model->eventTriggerConditions;
 
-		return true;
-	}
+    $record->save(false);
+    $model->id = $record->id;
 
-	public function getBlueprintById($id)
-	{
-		$record = Blueprint::findOne($id);
+    return true;
+  }
 
-		if (!$record) {
-			return null;
-		}
+  public function getBlueprintById($id)
+  {
+    $record = Blueprint::findOne($id);
 
-		$models = Courier::getInstance()
-			->modelPopulator
-			->populateModels(
-				[$record],
-				\refinery\courier\models\Blueprint::class
-			);
+    if (!$record) {
+      return null;
+    }
 
-		return $models[0];
-	}
+    $models = Courier::getInstance()
+      ->modelPopulator
+      ->populateModels(
+        [$record],
+        \refinery\courier\models\Blueprint::class
+      );
 
-	public function deleteBlueprintById($id)
-	{
+    return $models[0];
+  }
+
+  public function deleteBlueprintById($id)
+  {
     $record = Blueprint::findOne($id);
     $result = false;
 
@@ -110,47 +111,47 @@ class Blueprints extends Component
     }
 
     return $result;
-	}
+  }
 
-	public function checkEventConditions(Event $event, BlueprintModel $blueprint)
-	{
-		$renderVariables = [];
-		$renderVariables['event'] = $event;
-		$renderVariables['sender'] = $event->sender;
+  public function checkEventConditions(Event $event, BlueprintModel $blueprint)
+  {
+    $renderVariables = [];
+    $renderVariables['event'] = $event;
+    $renderVariables['sender'] = $event->sender;
 
-		$globalSets = \craft\elements\GlobalSet::find()
-			->anyStatus()
-			->all();
+    $globalSets = \craft\elements\GlobalSet::find()
+      ->anyStatus()
+      ->all();
 
-		foreach ($globalSets as $globalSet) {
-			$renderVariables[$globalSet->handle] = $globalSet;
-		}
+    foreach ($globalSets as $globalSet) {
+      $renderVariables[$globalSet->handle] = $globalSet;
+    }
 
-		try {
-			$eventTriggerConditions = \Craft::$app
-				->view
-				->renderString($blueprint->eventTriggerConditions, $renderVariables);
-		} catch (\Exception $e) {
-			// Log here
-			throw new \Exception($e);
-		}
+    try {
+      $eventTriggerConditions = \Craft::$app
+      ->view
+      ->renderString($blueprint->eventTriggerConditions, $renderVariables);
+    } catch (\Exception $e) {
+      // Log here
+      throw new \Exception($e);
+    }
 
-		$eventTriggerConditions = trim($eventTriggerConditions);
+    $eventTriggerConditions = trim($eventTriggerConditions);
 
     // If the trigger condition yields something other than "1" or "true",
     // return since the trigger is not valid in this case.
-		if($eventTriggerConditions !== "1" && $eventTriggerConditions !== "true") {
-			return;
-		}
+    if($eventTriggerConditions !== "1" && $eventTriggerConditions !== "true") {
+      return;
+    }
 
-		$courierEmailJob = new SendCourierEmailJob();
-		$courierEmailJob->renderVariables = $renderVariables;
-		$courierEmailJob->blueprintId = $blueprint->id;
-		$courierEmailJob->blueprintName = $blueprint->name;
+    $courierEmailJob = new SendCourierEmailJob();
+    $courierEmailJob->renderVariables = $renderVariables;
+    $courierEmailJob->blueprintId = $blueprint->id;
+    $courierEmailJob->blueprintName = $blueprint->name;
 
-		// Send email job
-		Craft::$app->queue->push($courierEmailJob);
+    // Send email job
+    Craft::$app->queue->push($courierEmailJob);
 
-		return true;
-	}
+    return true;
+  }
 }
